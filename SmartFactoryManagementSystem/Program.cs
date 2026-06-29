ebdb22729a4d4e24283a6d5416bdbc3f4e945044
training <training@TRAINING-007>
2026-06-29 11:11:50 +0300

    Add project files.

---
+++ SmartFactoryManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartFactoryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
