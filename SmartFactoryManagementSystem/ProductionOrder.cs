a5afbfdb48d3011dc141c0589b8c24617e7baeff
AndreiTurbinca <andreiturbinca2008@gmail.com>
2026-06-30 10:03:44 +0300

    test

---
+++ SmartFactoryManagementSystem/ProductionOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+public class ProductionOrder
+{
+	private int? orderId
+	{
+		get; set;
+	}
+	private int? targetQuantity
+	{
+        get; set;
+    }
+    private int? producedQuantity
+    {
+        get; set;
+    }
+    private enum Priority
+    {
+        Low ,Medium,High
+    }
+    private DateTime createddDate { get; set; }
+    bool isCompleted { get; set; }
+
+
+}
